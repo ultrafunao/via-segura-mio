2e341372e534dfab749ab78eb53137bc186422d1
ultrafunao <mancholamsamuel@gmail.com>
2026-05-18 23:09:04 -0500

    feat: Google Sign-In nativo Android con capacitor-social-login

--- vial-segura-app/android/app/src/main/java/com/usco/vialsegura/MainActivity.java
+++ vial-segura-app/android/app/src/main/java/com/usco/vialsegura/MainActivity.java
@@ -1,5 +1,33 @@
 package com.usco.vialsegura;
 
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginHandle;
+import ee.forgr.capacitor.social.login.GoogleProvider;
+import ee.forgr.capacitor.social.login.SocialLoginPlugin;
+import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
+import android.content.Intent;
+import android.util.Log;
+import java.util.List;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode >= GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MIN && requestCode < GoogleProvider.REQUEST_AUTHORIZE_GOOGLE_MAX) {
+      PluginHandle pluginHandle = getBridge().getPlugin("SocialLogin");
+      if (pluginHandle == null) {
+        Log.i("Google Activity Result", "SocialLogin login handle is null");
+        return;
+      }
+      Plugin plugin = pluginHandle.getInstance();
+      if (!(plugin instanceof SocialLoginPlugin)) {
+        Log.i("Google Activity Result", "SocialLogin plugin instance is not SocialLoginPlugin");
+        return;
+      }
+      ((SocialLoginPlugin) plugin).handleGoogleLoginIntent(requestCode, resultCode, data);
+    }
+  }
+}
